03eb071c976809ed7d3cee5ddefe43c3271e944e
Sandhya <sandhyatanwar2030@gmail.com>
2026-07-27 07:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.69%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0  # Pointer for the next non-val element
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
